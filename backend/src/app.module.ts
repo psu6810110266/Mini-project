29f404574dd90d0264e52f5281f2695feacb5f76
Phuminan Jivananthapravat <Phuminan.jiv@gmail.com>
2026-01-03 20:39:42 +0700

    Delete_Edit_feature

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. โหลดค่าจากไฟล์ .envป
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    
+    // 2. เชื่อมต่อ Database
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true, // เปิดเฉพาะตอน Dev (มันจะแก้ตารางให้อัตโนมัติ)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
